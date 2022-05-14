c19d3e61d9e43a3fb7fec752c964014c56ed92dd
Dmitriy Berezikov <dberezikov@msk.bcs.ru>
2022-05-14 04:48:44 +0000

    added new dynamic-inventory for ansible

--- ansible/dynamic-inventory.sh
+++ ansible/dynamic-inventory.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if [ "$1" == "--list" ] ; then
   if [ -e $inventory_temp ]; then
           echo "[all]" > inventory_temp
@@ -7,9 +6,18 @@ if [ "$1" == "--list" ] ; then
           touch inventory_temp
           echo "[all]" > inventory_temp
   fi
-  yc compute instance list | grep RUNNING | awk '{print$10}' | grep -v '^|' | sed -E '/^$/d' >> inventory_temp
+  yc compute instance list | grep RUNNING | awk '{print$4}' > inventory_host_temp
+  yc compute instance list | grep RUNNING | awk '{print$10}' > inventory_ip_temp
+  pr -mts' ansible_host=' inventory_host_temp inventory_ip_temp > pre-inventory_temp
+  sort -k 1 pre-inventory_temp >> sorted_inventory_temp
+  echo "[master]" >> inventory_temp
+  cat sorted_inventory_temp | grep node | grep node0 >> inventory_temp
+  echo "[workers]" >> inventory_temp
+  cat sorted_inventory_temp | grep node | grep -v node0 >> inventory_temp
+  echo "[no_groups]" >> inventory_temp
+  cat sorted_inventory_temp | grep -v node >> inventory_temp
   ansible-inventory --list -i inventory_temp
-  rm inventory_temp
+  rm inventory_host_temp inventory_ip_temp inventory_temp sorted_inventory_temp
 elif [ "$1" == "--host" ]; then
           echo '{"_meta": {"hostvars": {}}}'
   else
